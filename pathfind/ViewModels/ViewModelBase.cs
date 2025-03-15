d5a3a6162488523a3797aa1c2ef4b328e873f096
John <jbhumph@gmail.com>
2025-03-15 12:10:10 -0700

    Added Avalonia template.

---
+++ pathfind/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace pathfind.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ pathfind/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace pathfind.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
